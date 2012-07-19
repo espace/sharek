34210e646244ccea53a12e3dd1aca7d68e65e5dc
Alaa Moustafa <alaa.moustafa@gmail.com>
2012-07-19 10:53:41 +0200

    UI Applying

--- dostor/views.py
+++ dostor/views.py
@@ -13,10 +13,10 @@ def tag_detail(request, tag_slug):
     tags = Tag.objects.all
     tag = get_object_or_404( Tag, slug=tag_slug )
     articles = tag.article_set.all()
-    return render_to_response('tag_detail.html', {'tags':tags,'tag':tag,'articles': articles,},RequestContext(request))
+    return render_to_response('tag.html', {'tags':tags,'tag':tag,'articles': articles,},RequestContext(request))
 
 def article_detail(request, tag_slug, article_slug):
     tags = Tag.objects.all
     tag = get_object_or_404( Tag, slug=tag_slug )
     article = get_object_or_404( Article, slug=article_slug )
-    return render_to_response('article_detail.html', {'tags':tags,'tag':tag,'article': article,},RequestContext(request))
+    return render_to_response('article.html', {'tags':tags,'tag':tag,'article': article,},RequestContext(request))
