35f03a71358da36ba405e028a44bfe759407d0a9
Alaa Moustafa <alaa.moustafa@gmail.com>
2012-07-29 13:27:20 +0200

    Tags Ordering

--- dostor/views.py
+++ dostor/views.py
@@ -15,12 +15,13 @@ from dostor_masr import settings
 
 def index(request):
     user = None
+    home = True
     if request.user.is_authenticated():
       user = request.user
     tags = Tag.objects.all
     feedback_count = len(Feedback.objects.all())
-    perecent = int(feedback_count/5000)
-    home = True
+    perecent = int((float(feedback_count)/150)*100)
+    print perecent
     template_context = {'home':home,'tags':tags,'settings': settings,'user':user,'count':feedback_count,'perecent':perecent}
     return render_to_response('index.html', template_context ,RequestContext(request))
     
