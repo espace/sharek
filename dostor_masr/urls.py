1c63180fb87b6e6d515fb76dcd9ee6836d021ef7
Alaa Moustafa <alaa.moustafa@gmail.com>
2012-07-29 10:08:25 +0200

    Topics URL

--- dostor_masr/urls.py
+++ dostor_masr/urls.py
@@ -25,8 +25,8 @@ urlpatterns = patterns('',
     url(r'^tags/(?P<tag_slug>[-\w]+)/$', 'dostor.views.tag_detail'),
     #Topc detail URL
     
-    url(r'^topics/(?P<topic_slug>[-\w]+)/$', 'dostor.views.topic_detail'),
     url(r'^topics', 'dostor.views.topic_detail'),
+    url(r'^topics/(?P<topic_slug>[-\w]+)/$', 'dostor.views.topic_detail'),
     #Article detail URL
     url(r'^(?P<classified_by>[-\w]+)/(?P<class_slug>[-\w]+)/(?P<article_slug>[-\w]+)/$', 'dostor.views.article_detail'),
     url(r'^(?P<classified_by>[-\w]+)/(?P<class_slug>[-\w]+)/(?P<article_slug>[-\w]+)/(?P<order_by>[-\w]+)/$', 'dostor.views.article_detail'),
