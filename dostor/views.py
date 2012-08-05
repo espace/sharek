7447838ddef6fb278944c31ece899f241c9aca11
Alaa Moustafa <alaa.moustafa@gmail.com>
2012-08-05 14:32:48 +0200

    Facebook Connect

--- dostor/views.py
+++ dostor/views.py
@@ -199,6 +199,7 @@ def login(request):
 
             url = 'https://graph.facebook.com/oauth/access_token?' + \
                     urllib.urlencode(args)
+            print url
             response = cgi.parse_qs(urllib.urlopen(url).read())
             access_token = response['access_token'][0]
             expires = response['expires'][0]
