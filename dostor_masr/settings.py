51728407fbb1f9ad94faa0449bab9c33d670f1d0
Alaa Moustafa <alaa.moustafa@gmail.com>
2012-08-05 15:33:33 +0200

    Facebook Connect

--- dostor_masr/settings.py
+++ dostor_masr/settings.py
@@ -3,7 +3,7 @@
 FACEBOOK_APP_ID = '462028557155670'
 FACEBOOK_API_KEY = ''
 FACEBOOK_API_SECRET = 'be12f929d5bb8c51129b798f88548cfa'
-FACEBOOK_REDIRECT_URI = 'http://dostor-masr.espace-technologies.com:9090/facebook/login'
+FACEBOOK_REDIRECT_URI = 'http://dostor-masr.espace-technologies.com:9090/tags/security/article-66-71/'
 
 paginator = 10
 domain = "http://dostor-masr.espace-technologies.com:9090/"
