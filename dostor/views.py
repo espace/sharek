0cf48bce094c3665d39f98382a7eab66dac49b68
Alaa Moustafa <alaa.moustafa@gmail.com>
2012-08-05 15:29:33 +0200

    Facebook Connect

--- dostor/views.py
+++ dostor/views.py
@@ -1,3 +1,5 @@
+import os, sys
+
 from django.template import Context, loader, RequestContext
 from django.shortcuts  import render_to_response, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseNotFound
