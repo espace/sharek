64e113db7aff5ee6934ed4008b2edda0375ec486
Alaa Moustafa <alaa.moustafa@gmail.com>
2012-08-15 12:28:13 +0200

    change date format string

--- dostor/templatetags/date_format.py
+++ dostor/templatetags/date_format.py
@@ -15,18 +15,18 @@ def format(date):
             return "منذ يومين"
       elif diff.days < 1 and diff.seconds/3600 > 1:
           if diff.seconds/3600 == 1:
-            return "منذ ساعه"
+            return "منذ ساعة"
           elif diff.seconds/3600 == 2:
             return "منذ ساعتين"
           else:
-            return "منذ " + str(diff.seconds/3600) + " ساعات"
+            return "منذ " + str(diff.seconds/3600) + " ساعة"
       elif diff.seconds/3600 < 1:
           if diff.seconds%3600/60 == 1:
-            return "منذ دقيقه"
+            return "منذ دقيقة"
           elif diff.seconds%3600/60 == 2:
             return "منذ دقيقتين"
           else:
-            return "منذ " + str(diff.seconds%3600/60) + " دقائق"
+            return "منذ " + str(diff.seconds%3600/60) + " دقيقة"
       else:
         months = {1:"يناير", 2:"فبراير", 3:"مارس", 4:"إبريل", 5:"مايو", 6:"يونيو", 7:"يوليو", 8:"أغسطس", 9:"سبتمبر", 10:"أكتوبر", 11:"نوفمبر", 12:"ديسمبر"}
         return str(date.day)+"  " + months[date.month] +"  " +str(date.year)+",  الساعه "+str(date.hour)+":"+str(date.minute)
