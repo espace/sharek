8dac5d5ff11ae2b500e036cbc010953503fa5a80
Kareem <eng.kareem.hashem@gmail.com>
2012-08-22 18:11:44 +0200

    hilight the voted option

--- dostor/templatetags/hash.py
+++ dostor/templatetags/hash.py
@@ -6,5 +6,12 @@ def key(d, key_name):
       return d[key_name]
     else:
       return 0
-    
+
+def vote(d, key_name):
+    if d.__contains__(key_name):
+      return d[key_name].vote
+    else:
+      return 0
+
 key = register.filter('key', key)
+vote = register.filter('vote', vote)
