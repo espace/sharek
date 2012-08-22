c48b2ab8848b35b46c4b35681c18fb501441e93f
Kareem <eng.kareem.hashem@gmail.com>
2012-08-22 18:24:13 +0200

    hilight the voted option 2

--- dostor/templatetags/hash.py
+++ dostor/templatetags/hash.py
@@ -7,11 +7,12 @@ def key(d, key_name):
     else:
       return 0
 
-def vote(d, key_name):
-    if d.__contains__(key_name):
-      return d[key_name].vote
-    else:
-      return 0
+def vote(voted, key_name):
+  for comment_vote in voted:
+    if comment_vote.feedback_id == key_name:
+      return comment_vote.vote
+
+  return 0
 
 key = register.filter('key', key)
 vote = register.filter('vote', vote)
