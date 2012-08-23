cf0d09eab2c306165170402b9bb590f236dd6763
Kareem <eng.kareem.hashem@gmail.com>
2012-08-23 16:08:30 +0200

    admin can remove comment from the user interface

--- dostor/views.py
+++ dostor/views.py
@@ -182,7 +182,7 @@ def remove_feedback(request):
             feedback_id = request.POST.get("feedback")
             feedback = Feedback.objects.get(id=feedback_id)
             #the user has to be the feedback owner to be able to remove it
-            if feedback.user == request.user.username:
+            if feedback.user == request.user.username or request.user.username == "admin":
                 feedback.delete()
                 return HttpResponse(simplejson.dumps({'feedback_id':request.POST.get("feedback")}))
 
