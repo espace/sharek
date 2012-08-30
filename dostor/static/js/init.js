49f250c306e24626348fe2e682651ec42c955878
Alaa Moustafa <alaa.moustafa@gmail.com>
2012-08-30 15:48:20 +0200

    styling

--- dostor/static/js/init.js
+++ dostor/static/js/init.js
@@ -12,9 +12,9 @@ follow me if you like svg+js+html5+objc+cocos2d related topics
 */
 var prgress_indicator = $("#prgress_indicator").val();
 var prgress_Number = $("#prgress_Number").val();
-var text  = ' عدد المشاركات <br /><strong>' + prgress_Number + '</strong>'; 
+var text  = ' عدد المشاركات <br /><strong>'+prgress_Number+'</strong>'; 
 var title = $('#prgress_Percentage_Number').val();
-var text2 = ' <strong class="sec">' + title + '</strong>'; 
+var text2 = ' <strong class="sec">'+title+'</strong>'; 
 
 $(".fir").html(text); // to Set the number of mosharakat for the first time.
 
@@ -43,6 +43,7 @@ Raphael.fn.zeroToTenArc = function (x, y, radius, value) {
 var canvas = Raphael("canvas", 300, 250);
 
 var amin1 = canvas.zeroToTenArc(160, 130, 100, 10).attr({ stroke : "#eee", "stroke-width" : 12 });
+
 var amin2 = canvas.zeroToTenArc(160, 130, 100, prgress_indicator).attr({ stroke : "#79af01", "stroke-width" : 22 });
 
 canvas.circle(160, 130, 80).attr({ stroke: 'none', fill: '#e5e5e5' });
@@ -62,7 +63,7 @@ canvas.circle(160, 130, 80).attr({ stroke: 'none', fill: '#e5e5e5' });
 
 
             }).mouseout(function(){
-        this.stop().animate({ 'stroke-width': 22, opacity: 1 }, 250*4, 'elastic');
+        	this.stop().animate({ 'stroke-width': 22, opacity: 1 }, 250*4, 'elastic');
         
              $('.fir').stop().animate({ opacity: 0 }, 100, function(){
                   $(this).html(text).attr('title', text2).removeAttr('rel').animate({ opacity: 1 })
@@ -78,14 +79,14 @@ canvas.circle(160, 130, 80).attr({ stroke: 'none', fill: '#e5e5e5' });
               
               $('.fir').hover(function one (){
 
-                  $(this).stop().animate({ opacity: 0 }, 100, function(){
+                  $(this).stop().animate({ opacity: 0 }, 0, function(){
                       $(this).html(text2).attr('rel', text).removeAttr('title').animate({ opacity: 1 });
                   ;}); 
 
 
               },function two (){
 
-                 $(this).stop().animate({ opacity: 0 }, 100, function(){
+                 $(this).stop().animate({ opacity:0 }, 0, function(){
                   $(this).html(text).attr('title', text2).removeAttr('rel').animate({ opacity: 1 })
                   ;});     
               });
