0dc2697b5b2b0e285a1b11937baba8b68ee7e626
Alaa Moustafa <alaa.moustafa@gmail.com>
2012-08-30 11:57:47 +0200

    progress indicator

--- dostor/static/js/init.js
+++ dostor/static/js/init.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+
+
+   $(document).ready(function(){
         
 /*
 my answer to the following question:
@@ -8,6 +10,14 @@ me:
 http://twitter.com/basecode
 follow me if you like svg+js+html5+objc+cocos2d related topics
 */
+var prgress_indicator = $("#prgress_indicator").val();
+var prgress_Number = $("#prgress_Number").val();
+var text  = ' عدد المشاركات <br /><strong>' + prgress_Number + '</strong>'; 
+var title = $('#prgress_Percentage_Number').val();
+var text2 = ' <strong class="sec">' + title + '</strong>'; 
+
+$(".fir").html(text); // to Set the number of mosharakat for the first time.
+
 
 
 Raphael.fn.zeroToTenArc = function (x, y, radius, value) {
@@ -33,7 +43,7 @@ Raphael.fn.zeroToTenArc = function (x, y, radius, value) {
 var canvas = Raphael("canvas", 300, 250);
 
 var amin1 = canvas.zeroToTenArc(160, 130, 100, 10).attr({ stroke : "#eee", "stroke-width" : 12 });
-var amin2 = canvas.zeroToTenArc(160, 130, 100, 1).attr({ stroke : "#79af01", "stroke-width" : 22 });
+var amin2 = canvas.zeroToTenArc(160, 130, 100, prgress_indicator).attr({ stroke : "#79af01", "stroke-width" : 22 });
 
 canvas.circle(160, 130, 80).attr({ stroke: 'none', fill: '#e5e5e5' });
 
@@ -41,45 +51,47 @@ canvas.circle(160, 130, 80).attr({ stroke: 'none', fill: '#e5e5e5' });
 
 
 
-amin2.mouseover(function(){
-		this.animate({ 'stroke-width': 30, opacity: 0.75 }, 1000, 'elastic');
-this.toFront();
-	  
-	$('.fir').stop().animate({ opacity: 0 }, 100, function(){
-			  $(this).html(title).attr('rel', text).removeAttr('title').animate({ opacity: 1 });
-			  // $(this).html(text).attr('title', title).removeAttr('rel').animate({ opacity: 1 })
-		  ;}); 
+        amin2.mouseover(function(){
+                this.animate({ 'stroke-width': 30, opacity: 0.75 }, 1000, 'elastic');
+        this.toFront();
+              
+            $('.fir').stop().animate({ opacity: 0 }, 100, function(){
+                      $(this).html(text2).attr('rel', text).removeAttr('title').animate({ opacity: 1 });
+                  ;}); 
 
 
 
-	}).mouseout(function(){
-this.stop().animate({ 'stroke-width': 22, opacity: 1 }, 250*4, 'elastic');
+            }).mouseout(function(){
+        this.stop().animate({ 'stroke-width': 22, opacity: 1 }, 250*4, 'elastic');
+        
+             $('.fir').stop().animate({ opacity: 0 }, 100, function(){
+                  $(this).html(text).attr('title', text2).removeAttr('rel').animate({ opacity: 1 })
+                  ;});
 
-	 $('.fir').stop().animate({ opacity: 0 }, 100, function(){
-		  $(this).html(text).attr('title', title).removeAttr('rel').animate({ opacity: 1 })
-		  ;});
-
-	});
-	  
-	  $('.fir').wrapInner('<span />');
-	  
-	  $('.fir').hover(function one (){
-
-		  $(this).stop().animate({ opacity: 0 }, 100, function(){
-			  $(this).html(title).attr('rel', text).removeAttr('title').animate({ opacity: 1 });
-			  // $(this).html(text).attr('title', title).removeAttr('rel').animate({ opacity: 1 })
-		  ;}); 
+            });
 
 
-	  },function two (){
+            // function swapTitleAttr  () {
+             
+              
+            
+              
+              $('.fir').hover(function one (){
 
-		 $(this).stop().animate({ opacity: 0 }, 100, function(){
-		  $(this).html(text).attr('title', title).removeAttr('rel').animate({ opacity: 1 })
-		  ;});     
-	  });
+                  $(this).stop().animate({ opacity: 0 }, 100, function(){
+                      $(this).html(text2).attr('rel', text).removeAttr('title').animate({ opacity: 1 });
+                  ;}); 
 
 
-});//dom
+              },function two (){
+
+                 $(this).stop().animate({ opacity: 0 }, 100, function(){
+                  $(this).html(text).attr('title', text2).removeAttr('rel').animate({ opacity: 1 })
+                  ;});     
+              });
+
+
+        });//dom
 
 
 
