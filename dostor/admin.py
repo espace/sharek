03947e5267faa7d5499c7ed1b3394603c476bbce
Alaa Moustafa <alaa.moustafa@gmail.com>
2012-09-01 20:16:09 +0200

    Readonly

--- dostor/admin.py
+++ dostor/admin.py
@@ -2,7 +2,7 @@ from dostor.models import Tag
 from dostor.models import Article
 from dostor.models import Topic
 from dostor.models import Info, Feedback
-import ReadOnlyAdminFields
+from dostor.models import ReadOnlyAdminFields
 
 from django.contrib import admin
 
@@ -54,40 +54,7 @@ class InfoAdmin(admin.ModelAdmin):
     class Media:
         js = ( 'js/jquery.min.js', 'js/jquery-ui.min.js', 'js/admin-list-reorder.js', )
 
-from django import forms
 
-class ReadOnlyWidget(forms.Widget):
-    def __init__(self, original_value, display_value):
-        self.original_value = original_value
-        self.display_value = display_value
-
-        super(ReadOnlyWidget, self).__init__()
-
-    def render(self, name, value, attrs=None):
-        if self.display_value is not None:
-            return unicode(self.display_value)
-        return unicode(self.original_value)
-
-    def value_from_datadict(self, data, files, name):
-        return self.original_value
-
-class ReadOnlyAdminFields(object):
-    def get_form(self, request, obj=None):
-        form = super(ReadOnlyAdminFields, self).get_form(request, obj)
-
-        if hasattr(self, 'readonly'):
-            for field_name in self.readonly:
-                if field_name in form.base_fields:
-
-                    if hasattr(obj, 'get_%s_display' % field_name):
-                        display_value = getattr(obj, 'get_%s_display' % field_name)()
-                    else:
-                        display_value = None
-
-                    form.base_fields[field_name].widget = ReadOnlyWidget(getattr(obj, field_name, ''), display_value)
-                    form.base_fields[field_name].required = False
-
-        return form
     
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Tag, TagAdmin)
--- dostor/models.py
+++ dostor/models.py
@@ -151,3 +151,39 @@ class Info(models.Model):
     
     def get_absolute_url(self):
         return "/%s/" % (self.slug)
+
+
+from django import forms
+
+class ReadOnlyWidget(forms.Widget):
+    def __init__(self, original_value, display_value):
+        self.original_value = original_value
+        self.display_value = display_value
+
+        super(ReadOnlyWidget, self).__init__()
+
+    def render(self, name, value, attrs=None):
+        if self.display_value is not None:
+            return unicode(self.display_value)
+        return unicode(self.original_value)
+
+    def value_from_datadict(self, data, files, name):
+        return self.original_value
+
+class ReadOnlyAdminFields(object):
+    def get_form(self, request, obj=None):
+        form = super(ReadOnlyAdminFields, self).get_form(request, obj)
+
+        if hasattr(self, 'readonly'):
+            for field_name in self.readonly:
+                if field_name in form.base_fields:
+
+                    if hasattr(obj, 'get_%s_display' % field_name):
+                        display_value = getattr(obj, 'get_%s_display' % field_name)()
+                    else:
+                        display_value = None
+
+                    form.base_fields[field_name].widget = ReadOnlyWidget(getattr(obj, field_name, ''), display_value)
+                    form.base_fields[field_name].required = False
+
+        return form
