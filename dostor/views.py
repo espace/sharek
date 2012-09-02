35f82ea285e7881f96694040f6420bf4ee1d1265
Alaa Moustafa <alaa.moustafa@gmail.com>
2012-09-02 23:35:46 +0200

    Top Users

--- dostor/views.py
+++ dostor/views.py
@@ -39,6 +39,7 @@ def index(request):
       user = request.user
     topics = Topic.objects.all
 
+    connection._rollback()
     top_users = Article.objects.values('name').annotate(user_count=Count('name')).order_by('-user_count')[:5]
     print top_users
 
