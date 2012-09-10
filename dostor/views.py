79558e7d4c15ade49c3d9ebb18b67c2d87c562b0
Kareem <eng.kareem.hashem@gmail.com>
2012-09-10 14:56:51 +0200

    facebook post link

--- dostor/views.py
+++ dostor/views.py
@@ -301,7 +301,7 @@ def modify(request):
             # GraphAPI is the main class from facebook_sdp.py
             graph = facebook_sdk.GraphAPI(fb_user.access_token)
             attachment = {}
-            attachment['link'] = settings.domain+"sharek/"+request.POST.get("class_slug")+"/"+request.POST.get("article_slug")
+            attachment['link'] = settings.domain+"sharek/"+request.POST.get("class_slug")+"/"+request.POST.get("article_slug")+"/"
             attachment['picture'] = settings.domain+settings.STATIC_URL+"images/facebook.png"
             message = 'لقد شاركت في كتابة #دستور_مصر وقمت بالتعليق على '+get_object_or_404(ArticleDetails, id=request.POST.get("article")).header.name.encode('utf-8')+" من الدستور"
             graph.put_wall_post(message, attachment)
