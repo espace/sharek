22de1ae3cf18b561be5e3b5a4f0b41a45486f2b8
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-09-12 14:58:15 +0200

    Fix facebook.backend

--- sharek/urls.py
+++ sharek/urls.py
@@ -21,9 +21,9 @@ urlpatterns = patterns('',
     
     url(r'^sharek/admin/', include(admin.site.urls)),
     
-    url(r'^sharek/migrate/$', 'core.views.migrate', name='migrate'),
-    url(r'^sharek/migrate_db/$', 'core.views.migrate_db', name='migrate_db'),
-    url(r'^sharek/migrate_tags/$', 'core.views.migrate_tags', name='migrate_tags'),
+    #url(r'^sharek/migrate/$', 'core.views.migrate', name='migrate'),
+    #url(r'^sharek/migrate_db/$', 'core.views.migrate_db', name='migrate_db'),
+    #url(r'^sharek/migrate_tags/$', 'core.views.migrate_tags', name='migrate_tags'),
 
     url(r'^sharek/slider/$', 'core.views.slider', name='slider'),
     url(r'^sharek/search/$', 'core.views.search', name='search'),
