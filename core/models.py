f939e7dda824974bf6bb17c6f87b6949d79f4286
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-09-16 15:31:05 +0200

    Adding Search in Admin Models

--- core/models.py
+++ core/models.py
@@ -113,7 +113,7 @@ class ArticleDetails(models.Model):
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
     current = models.BooleanField(default=False)
-    mod_date = models.DateTimeField(default=timezone.make_aware(datetime.now(),timezone.get_default_timezone()).astimezone(timezone.utc), verbose_name='Last Modification Date')
+    mod_date = models.DateTimeField(default=timezone.make_aware(datetime.now(),timezone.get_default_timezone()).astimezone(timezone.utc), verbose_name='Modification Date')
 
     def feedback_count(self):
         inactive_users = User.get_inactive
@@ -160,8 +160,8 @@ class ArticleDetails(models.Model):
       return ArticleDetails.objects.filter(current = True).annotate(num_feedbacks=Count('feedback')).order_by('-num_feedbacks')[:limit]
 
     class Meta:
-       verbose_name = "Article detail"
-       verbose_name_plural = "Article details"
+       verbose_name = "Article Text"
+       verbose_name_plural = "Article Texts"
 
 exclusive_boolean_fields(ArticleDetails, ('current',), ('header',))
 
@@ -177,7 +177,7 @@ class Article(models.Model):
     dislikes = models.IntegerField(default=0)
     original = models.ForeignKey("self", null = True, blank = True)
     current = models.BooleanField(default=False)
-    mod_date = models.DateTimeField(default=timezone.make_aware(datetime.now(),timezone.get_default_timezone()).astimezone(timezone.utc), verbose_name='Last Modification Date')
+    mod_date = models.DateTimeField(default=timezone.make_aware(datetime.now(),timezone.get_default_timezone()).astimezone(timezone.utc), verbose_name='Modification Date')
     
     
     def feedback_count(self):
