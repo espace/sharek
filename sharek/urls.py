4be88ac1a90a1b0ab87106f5708621d3b079b940
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-09-19 17:14:36 +0200

    Index Image linhk

--- sharek/urls.py
+++ sharek/urls.py
@@ -21,6 +21,9 @@ urlpatterns = patterns('',
 
     url(r'^sharek/slider/$', 'core.views.slider', name='slider'),
     url(r'^sharek/search/$', 'core.views.search', name='search'),
+	
+	url(r'^sharek/migrate/$', 'core.views.migrate', name='migrate'),
+    url(r'^sharek/migrate_images/$', 'core.views.migrate_images', name='migrate_images'),
     
     url(r'^sharek/vote/', 'core.views.vote', name='vote'),
     url(r'^sharek/article_vote/', 'core.views.article_vote', name='article_vote'),
