a6af214d0d577a7821319af96c91954aa9e6b18f
Kareem <eng.kareem.hashem@gmail.com>
2012-09-19 16:05:48 +0200

    map no of activities be dynamic not hardcoded 2

--- core/views.py
+++ core/views.py
@@ -597,6 +597,7 @@ def top_users_map(request):
 
     top_users = []
     inactive_users = User.get_inactive
+    counter = 0
     if user == None:
         temp_users = Feedback.objects.values('user').annotate(user_count=Count('user')).order_by('?').exclude(user__in=inactive_users)[:2000]
     else:
