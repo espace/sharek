eef04f4f5e96eca8851c16bbd1d5c6eaa3f6a9b6
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-09-20 11:42:22 +0200

    Auto post to FB Page

--- core/facebook/views.py
+++ core/facebook/views.py
@@ -80,7 +80,7 @@ def auto_post(request):
 
     for article in articles:
 
-        message = article.header.topic.name.encode('utf-8') + " - " + article.header.name.encode('utf-8') + "&rlm; \n" + article.summary
+        message = article.header.topic.name.encode('utf-8') + " - " + article.header.name.encode('utf-8') + "\n" + article.summary.encode('utf-8')
 
         attachment = {}
         attachment['name'] = article.header.topic.name.encode('utf-8') + " - " + article.header.name.encode('utf-8') + "&rlm;"
