c8efb70ec20acc737558158f56838e929ed43eeb
Kareem <eng.kareem.hashem@gmail.com>
2012-09-23 15:03:01 +0200

    fix the current article issue in the admin to be like radio button

---
+++ core/static/js/admin-current-article.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $(".field-current").live("click",function(){
+    
+    $action = $($(this).children()[0])
+    current_state = $action.is(':checked');
+    //var fields_current = $('field-current');
+    //var arr = $.makeArray(fields_current);
+    $('.field-current').each(function(index) {
+      var $temp = $($(this).children()[0])
+      $temp.val(false);  
+      $temp.attr('checked', false);
+    });
+    
+    var $current = $($(this).children()[0])
+    if (current_state == "on" || current_state == true){  
+      $current.val(true);
+      $current.attr('checked', true);
+    }else{
+      $current.val(false);
+      $current.attr('checked', false);
+    }
+  });
+});
