4076822babd31c3182e8efdbd540e4e254ca668d
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-09-23 16:03:18 +0200

    trying exporting to pdf

--- core/reports/views.py
+++ core/reports/views.py
@@ -10,7 +10,7 @@ from core.models import Feedback, Rating, Topic
 from core.models import Info, ArticleRating
 from core.views import login
 
-from wkhtmltopdf import render_to_pdf
+#from wkhtmltopdf import render_to_pdf
 
 from operator import attrgetter
 
