aced0fbc2e1f2257280e41167bb742c6982ea3fa
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-09-23 18:46:34 +0200

    trying exporting to pdf

--- core/reports/views.py
+++ core/reports/views.py
@@ -16,16 +16,16 @@ from core.models import Feedback, Rating, Topic
 from core.models import Info, ArticleRating
 from core.views import login
 
-from wkhtmltopdf import render_to_pdf
+#from wkhtmltopdf import render_to_pdf
 
 from operator import attrgetter
 from sharek import settings
 
-
+'''
 def pdf(request):
     return render_to_pdf(request, 'reports/template.html')
-
-def ddd(request):
+''' 
+def pdf(request):
     template = loader.get_template('reports/template.html')
     context = Context({'user':request.user,'msg':'Testing sample PDF creation'})
     rendered = template.render(context)
