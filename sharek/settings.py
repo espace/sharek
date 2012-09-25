944974040f9760f4e2ef8f65a9cd0314082c3afb
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-09-25 15:40:18 +0200

    Cleaning Settings.py file

--- sharek/settings.py
+++ sharek/settings.py
@@ -71,12 +71,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/sharek/static/photos/'
+MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://192.168.1.103:8000/sharek/static/photos/'
+MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
