5a050b677152dadc1516f4c5758d04ba10459914
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-09-25 18:48:35 +0200

    Optimizing DB Connections

--- core/models.py
+++ core/models.py
@@ -197,6 +197,8 @@ class Feedback(models.Model):
     order = models.IntegerField(default=0)
     user = models.CharField(max_length=200,default='')
     articledetails = models.ForeignKey(ArticleDetails, null = True, blank = True)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
 
     def get_children(self):
         inactive_users = User.get_inactive
