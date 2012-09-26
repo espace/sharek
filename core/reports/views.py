486b946c8e529c4956a5fca16332b46c98f05f49
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-09-26 12:23:57 +0200

    Changing PDF margins

--- core/reports/views.py
+++ core/reports/views.py
@@ -134,7 +134,7 @@ def render_to_pdf(template_html, template_prefix, context, pdf_filename):
      file.write(rendered.encode('utf8'))
      file.close( )
 
-     command_args = 'wkhtmltopdf --footer-html ' + core.__path__[0] + '/static/footer.html --margin-top 15 --header-spacing 30 --margin-bottom 15 --footer-spacing 30 ' + full_temp_html_file_name + ' -'
+     command_args = 'wkhtmltopdf --footer-html ' + core.__path__[0] + '/static/footer.html --margin-top 15 --margin-bottom 15 --footer-spacing 20 ' + full_temp_html_file_name + ' -'
      popen = subprocess.Popen(command_args, bufsize=4096, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
      pdf_contents = popen.stdout.read()
      popen.terminate()
