2f8958b035dbc234a9aae965d9ea9afaf074d524
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-09-26 11:53:37 +0200

    Adding Header & Footer to the generated PDF

--- core/reports/views.py
+++ core/reports/views.py
@@ -134,7 +134,7 @@ def render_to_pdf(template_html, template_prefix, context, pdf_filename):
      file.write(rendered.encode('utf8'))
      file.close( )
 
-     command_args = 'wkhtmltopdf --footer-html ' + core.__path__[0] + '/static/footer.html ' + full_temp_html_file_name + ' -'
+     command_args = 'wkhtmltopdf --footer-spacing 10 --footer-html ' + core.__path__[0] + '/static/footer.html ' + full_temp_html_file_name + ' -'
      popen = subprocess.Popen(command_args, bufsize=4096, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
      pdf_contents = popen.stdout.read()
      popen.terminate()
