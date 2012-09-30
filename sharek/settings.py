364406ab89a88fd1462ef2296e828886dcfff643
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-09-30 22:04:03 +0200

    Bug Fixing

--- sharek/settings.py
+++ sharek/settings.py
@@ -120,6 +120,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+	'snippetscream.ProfileMiddleware',
     #'sugar.middleware.speedtracer.SpeedTracerMiddleware',
 )
 
