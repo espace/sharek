a0ba12d201e4c4aea4c585f2d5d943c54f4609b3
Kareem <eng.kareem.hashem@gmail.com>
2012-10-03 13:17:58 +0200

    remove sugar from settings

--- sharek/settings.py
+++ sharek/settings.py
@@ -153,7 +153,6 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'sugar',
     'debug_toolbar',
 )
 
