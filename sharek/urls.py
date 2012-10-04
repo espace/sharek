911e9a8056079832b690624eaacd9d0d79a2c627
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-10-04 18:23:21 +0200

    Moving Debug setting to settings_local.py

--- sharek/urls.py
+++ sharek/urls.py
@@ -70,7 +70,7 @@ urlpatterns = patterns('',
     url(r'^sharek/profile/(?P<browsing_data>[-\w]+)/$', 'core.profiles.views.profile', name='profile'),
     url(r'^sharek/user_profile/$', 'core.profiles.views.user_profile', name='user_profile'),
 
-    url(r'^sharek/', 'core.views.index', name='index'),
+    url(r'^sharek/$', 'core.views.index', name='index'),
 	
     url(r'^sharek/', include('core.social_auth.urls')),
 	
