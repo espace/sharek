818b9bc2c391d8f7059bb55b1a04f35962ba0f4a
Kareem <eng.kareem.hashem@gmail.com>
2012-10-31 12:39:50 +0200

    fb comment post solve 2

--- core/views.py
+++ core/views.py
@@ -399,8 +399,8 @@ def modify(request):
             
             if request.user.username != "admin":
                 # post on twitter or facebook
-                if UserSocialAuth.auth_provider(request.user.id) == 'facebook':
-                    fb_user = FacebookSession.objects.get(user = request.user.id)
+                if UserSocialAuth.auth_provider(request.user) == 'facebook':
+                    fb_user = FacebookSession.objects.get(user = request.user)
                     # GraphAPI is the main class from facebook_sdp.py
                     graph = facebook_sdk.GraphAPI(fb_user.access_token)
                     attachment = {}
