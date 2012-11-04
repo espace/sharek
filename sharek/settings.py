7908a68b095ade49156d49dd169ee9bc9ef0ba97
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-11-04 17:55:02 +0200

    Changing logging in popup height

--- sharek/settings.py
+++ sharek/settings.py
@@ -8,7 +8,7 @@ except ImportError:
 import os
 
 LOGIN_URL          = '/sharek/login-form/'
-LOGIN_REDIRECT_URL = '/sharek/logged-in/'
+LOGIN_REDIRECT_URL = '/'
 LOGIN_ERROR_URL    = '/sharek/login-error/'
 
 SOCIAL_AUTH_COMPLETE_URL_NAME  = 'socialauth_complete'
