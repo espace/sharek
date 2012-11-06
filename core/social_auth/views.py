a48b3a3cfc156f15ddf47fd6a56fd3b5ff696956
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-11-06 12:13:54 +0200

    Twitter Login fixing

--- core/social_auth/views.py
+++ core/social_auth/views.py
@@ -38,7 +38,6 @@ def auth(request, backend):
 def complete(request, backend, *args, **kwargs):
     """Authentication complete view, override this view if transaction
     management doesn't suit your needs."""
-    print request.user.is_authenticated()
     if request.user.is_authenticated():
         return associate_complete(request, backend, *args, **kwargs)
     else:
@@ -101,6 +100,8 @@ def complete_process(request, backend, *args, **kwargs):
     # pop redirect value before the session is trashed on login()
     redirect_value = request.session.get(REDIRECT_FIELD_NAME, '')
     user = auth_complete(request, backend, *args, **kwargs)
+
+    print 'complete'
 
     if isinstance(user, HttpResponse):
         return user
