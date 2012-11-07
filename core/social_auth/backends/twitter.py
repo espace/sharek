385e30cd5bb4171a3424c726b1b9614214f2937f
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-11-07 12:27:10 +0200

    Twitter Login fixing

--- core/social_auth/backends/twitter.py
+++ core/social_auth/backends/twitter.py
@@ -80,7 +80,7 @@ class TwitterAuth(ConsumerBasedOAuth):
 
     def auth_complete(self, *args, **kwargs):
         """Completes login process, must return user instance"""
-        print "sdf"
+        print self.data
         if 'denied' in self.data:
             raise AuthCanceled(self)
         else:
