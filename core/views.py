218e987dd638af4e5f2efa796af72d7ab99ca3e6
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-11-07 15:58:51 +0200

    Twitter Login Fixing

--- core/views.py
+++ core/views.py
@@ -404,11 +404,11 @@ def modify(request):
                     access_token = extra_data['access_token']
                  # GraphAPI is the main class from facebook_sdp.py
                     graph = facebook_sdk.GraphAPI(access_token)
-                 attachment = {}
-                 attachment['link'] = settings.domain+"sharek/"+request.POST.get("class_slug")+"/"+request.POST.get("article_slug")+"/"
-                 attachment['picture'] = settings.domain + settings.STATIC_URL + "images/facebook.png"
-                 message = 'لقد شاركت في كتابة #دستور_مصر وقمت بالتعليق على '+get_object_or_404(ArticleDetails, id=request.POST.get("article")).header.name.encode('utf-8')+" من الدستور"
-                 graph.put_wall_post(message, attachment)
+                    attachment = {}
+                    attachment['link'] = settings.domain+"sharek/"+request.POST.get("class_slug")+"/"+request.POST.get("article_slug")+"/"
+                    attachment['picture'] = settings.domain + settings.STATIC_URL + "images/facebook.png"
+                    message = 'لقد شاركت في كتابة #دستور_مصر وقمت بالتعليق على '+get_object_or_404(ArticleDetails, id=request.POST.get("article")).header.name.encode('utf-8')+" من الدستور"
+                    graph.put_wall_post(message, attachment)
             
                 if UserSocialAuth.auth_provider(request.user.username) == 'twitter':
                     extra_data = UserSocialAuth.get_extra_data(request.user.username)
