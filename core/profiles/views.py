a3bf661f0993c85ae58f6c11fda68cf94bf80935
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-11-08 12:41:06 +0200

    Fixing Map Issue

--- core/profiles/views.py
+++ core/profiles/views.py
@@ -45,7 +45,7 @@ def profile(request, browsing_data="def"):
             if id['articledetails'] != None:
                 temp = ArticleDetails.objects.get(id = id['articledetails'])
                 feedbacks = Feedback.objects.filter(articledetails_id = id['articledetails'], parent_id = None, user = user)
-                commented_articles.append({'topic':temp.header.topic,'name':temp.header.name,'url':"#",'feedbacks':feedbacks})
+                commented_articles.append({'topic':temp.header.topic,'name':temp.header.name,'slug':temp.slug,'feedbacks':feedbacks})
                 
 
     return render_to_response('profile.html', {'voted_articles': voted_articles, 'profile':True,'browsing_data':browsing_data,'commented_articles':commented_articles,'disliked_articles':disliked_articles,'liked_articles':liked_articles,'settings': settings,'user':user} ,RequestContext(request))
