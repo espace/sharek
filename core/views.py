48780b8fd8fc65731714380cacf018e7d558b342
Kareem <eng.kareem.hashem@gmail.com>
2012-11-11 16:48:36 +0200

    As a User I can visit a comment with its replies in a seperate page 6

--- core/views.py
+++ core/views.py
@@ -427,7 +427,8 @@ def modify(request):
                                       consumer_secret=settings.TWITTER_CONSUMER_SECRET,
                                       access_token_key=access_token_key,
                                       access_token_secret=access_token_secret)
-                    link = shorten_url(urlencode(settings.domain+"sharek/"+request.POST.get("class_slug")+"/"+request.POST.get("article_slug")+"/comment/"+str(feedback[0].id)+"/"))
+                    temp = str(settings.domain+"sharek/"+request.POST.get("class_slug")+"/"+request.POST.get("article_slug")+"/comment/"+str(feedback[0].id)+"/")
+                    link = shorten_url(urlencode(temp))
                     message = 'لقد شاركت في كتابة #دستور_مصر وقمت بالتعليق على '+get_object_or_404(ArticleDetails, id=request.POST.get("article")).header.name.encode('utf-8')+ link + "  من الدستور"
                     api.PostUpdate(message)
 
