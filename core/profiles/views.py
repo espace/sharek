bd74d4081589ac99a61b9f3553afc0b45889f28d
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-11-12 11:05:21 +0200

    Profile Page Design Enhancements

--- core/profiles/views.py
+++ core/profiles/views.py
@@ -31,6 +31,8 @@ def profile(request, browsing_data="def"):
     disliked_articles = []
     commented_articles = []
 
+	contributions = 12
+
     voted_articles = ArticleRating.objects.filter(user = user)
 
     if browsing_data == "likes" or browsing_data == "def":
@@ -46,8 +48,6 @@ def profile(request, browsing_data="def"):
                 temp = ArticleDetails.objects.get(id = id['articledetails'])
                 feedbacks = Feedback.objects.filter(articledetails_id = id['articledetails'], parent_id = None, user = user)
                 commented_articles.append({'topic':temp.header.topic,'name':temp.header.name,'slug':temp.slug,'feedbacks':feedbacks})
-
-	contributions = 12
                 
 
     return render_to_response('profile.html', {'voted_articles': voted_articles, 'contributions': contributions, 'profile':True,'browsing_data':browsing_data,'commented_articles':commented_articles,'disliked_articles':disliked_articles,'liked_articles':liked_articles,'settings': settings,'user':user} ,RequestContext(request))
