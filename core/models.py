ce4e76f253a8a0ac9f49293806f1dd61ed7802cc
Alaa Moustafa <alaa.moustafa@espace.com.eg>
2012-12-02 19:49:10 +0200

    Fixing Tags Query

--- core/models.py
+++ core/models.py
@@ -39,16 +39,14 @@ class Tag(models.Model):
 						core_articledetails.likes, core_articledetails.dislikes, core_articledetails.mod_date, core_articledetails.feedback_count,
 						core_articleheader.chapter_id, core_chapter.name, core_articleheader.branch_id, core_branch.name, core_topic.name,
 						core_articledetails.original, original_articledetails.slug
-					FROM core_articleheader
-					INNER JOIN core_articledetails ON core_articleheader.id = core_articledetails.header_id
+					FROM core_articleheader_tags
+					INNER JOIN core_articleheader ON core_articleheader.id = core_articleheader_tags.articleheader_id
+					INNER JOIN core_articledetails ON core_articleheader.id = core_articledetails.header_id AND core_articledetails.current IS TRUE
 					INNER JOIN core_articledetails original_articledetails ON original_articledetails.id = core_articledetails.original
-					INNER JOIN core_articleheader_tags ON core_articleheader.id = core_articledetails.header_id
-					INNER JOIN core_tag ON core_tag.id = core_articleheader_tags.tag_id
-					INNER JOIN core_topic ON core_articleheader.id = core_articleheader_tags.articleheader_id
+					INNER JOIN core_topic ON core_topic.id = core_articleheader.topic_id
 					LEFT JOIN core_chapter ON core_articleheader.chapter_id = core_chapter.id
 					LEFT JOIN core_branch ON core_articleheader.branch_id = core_branch.id
-					WHERE core_articledetails.current IS TRUE AND core_tag.id = %s
-					ORDER BY coalesce(core_chapter.order, 0), coalesce(core_branch.order, 0), core_articleheader.order'''
+					WHERE tag_id = %s ORDER BY core_topic.order, coalesce(core_chapter.order, 0), coalesce(core_branch.order, 0), core_articleheader.order'''
 
        if offset != None and limit != None:
            query = query + ' OFFSET ' + str(offset) + ' LIMIT ' + str(limit)
